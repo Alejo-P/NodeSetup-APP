eb508b7cceeb84fc4a694b8486d00eea62519ea9
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-12-09 00:27:03 -0500

    Añadir NpmService para ejecutar comandos de npm con manejo de errores y opciones de ventana

--- services/npm_service.py
+++ services/npm_service.py
@@ -2,7 +2,26 @@ import subprocess
 import os
 import json
 
+from Actions import getPathOf
+
 # Servicio para ejecutar comandos de npm
 class NpmService:
-    def __init__(self):
-        pass
+    npm_path = getPathOf('npm')
+    def __init__(self, path:str = os.getcwd()):
+        self.path = path
+    
+    def run(self, command:list[str], newWindow:bool = False, in_bytes:bool = False):
+        try:
+            resultado = subprocess.run(
+                command,
+                check=True,
+                cwd=self.path,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=not in_bytes,
+                creationflags=subprocess.CREATE_NO_WINDOW if not newWindow else 0   # Evita que se abra una ventana de consola
+            )
+            return resultado.stdout
+        except subprocess.CalledProcessError as error:
+            return error.stderr
+        
